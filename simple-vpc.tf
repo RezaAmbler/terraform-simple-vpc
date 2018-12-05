f5b898cf7ad305ec451d23ab0c6fc00c31a2d6f6
Reza Ambler <reza.ambler@gmail.com>
2018-12-05 15:17:11 -0800

    protocol update
    update egress to allow all outbound data.

--- simple-vpc.tf
+++ simple-vpc.tf
@@ -409,7 +409,7 @@ resource "aws_security_group" "web-app-sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
